6069096661f1cedb3a02ef539c889f202ad4e040
rayetata <zephaniahraye.belmis-24@cpu.edu.ph>
2026-08-19 17:15:18 +0800

    status: Ongoing

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { pool } from "./db";
+import customerRoutes from "./routes/customers";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "E-Commerce & Logistics API is running",
+  });
+});
+
+app.get("/api/v1/products", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM product");
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
+//Customer routes
+app.use("/api/v1/customers", customerRoutes);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
